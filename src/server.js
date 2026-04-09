84b33b172ca34cd5848460af6bcc7c9cc2b5b985
Jose Tondin <josevictortondin2006@gmail.com>
2026-04-08 21:46:59 -0300

    Estudos de Casa

--- src/server.js
+++ src/server.js
@@ -16,10 +16,10 @@ const server = http.createServer((req, res) => {
             email: "josevictortondin2006@gmail.com",
         })
 
-        return res.end("Criando Usuario")
+        return res.writeHead(201).end()
     }
 
-    return res.end("Hello World")
+    return res.writeHead(404).end()
 })
 
 server.listen(3333)
---
+++ streams/fundamentals.js
@@ -0,0 +1,38 @@
+/*Fundamentos de Streams*/ 
+
+import { Readable, Writable, Transform } from "node:stream";
+
+class OneToHundredStream extends Readable {
+    index = 1
+    _read() {
+        const i = this.index++
+
+        setTimeout(() => {
+            if (i > 100) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
+
+                this.push((buf + "\n"))
+            }            
+        }, 500);
+    }
+}
+
+class multiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString() * 10));
+        callback();
+    }
+}
+
+class modulonegativo extends Transform {
+    _transform(chunk, encoding, callback) {
+        const number = Number(chunk.toString()) * -1
+        callback(null, Buffer.from(String(number)))
+    }
+}
+
+
+
+new OneToHundredStream().pipe(new modulonegativo()).pipe(new multiplyByTenStream())

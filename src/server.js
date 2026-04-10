1ce0f5abfe17ddeee53d0048ec45102990517d80
Jose Tondin <josevictortondin2006@gmail.com>
2026-04-10 12:31:31 -0300

    Estudos de casa

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import http from 'node:http'
+import { randomUUID } from "node:crypto"
 import { json } from './middlewares/json.js'
+import { Database } from './middlewares/database.js'
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
     const {method, url} = req
@@ -9,18 +11,23 @@ const server = http.createServer(async (req, res) => {
     await json(req, res)
 
     if (method === "GET" && url === "/users") {
+        const users = database.select("users")
+
         return res.end(JSON.stringify(users))
     }
 
     if (method === "POST" && url === "/users") {
 
-        const {name, email} = req.fullStreamContent
+        const { name, email } = req.fullStreamContent
 
-        users.push({
-            id: 1,
+        const user = {
+            //GERA UM ID ALEATORIO E UNICO BASICAMENTE.
+            id: randomUUID(),
             name,
             email,
-        })
+        }
+
+        database.insert("users", user)
 
         return res.writeHead(201).end()
     }
